daf3901bbb3cbfd5b46f104f6e2de24f2bf2988e
Konrad Jäger <konrad.jaeger@hotmail.com>
2025-12-28 14:27:01 +0100

    Add root route for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,9 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { UserProvider } from "@/contexts/UserContext";
-import UserPicker from "./pages/UserPicker";
-import RoutePage from "./pages/RoutePage";
-import PlacesPage from "./pages/PlacesPage";
-import TodosPage from "./pages/TodosPage";
-import LogPage from "./pages/LogPage";
-import NotFound from "./pages/NotFound";
+import { Routes, Route } from "react-router-dom";
 
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <UserProvider>
-        <Toaster />
-        <Sonner />
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<UserPicker />} />
-            <Route path="/route" element={<RoutePage />} />
-            <Route path="/places" element={<PlacesPage />} />
-            <Route path="/todos" element={<TodosPage />} />
-            <Route path="/log" element={<LogPage />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </UserProvider>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<div>APP LÄUFT</div>} />
+    </Routes>
+  );
+}
