3e1e819e837cd608d71d28714fc8d6b2d6a5a0a5
Konrad Jäger <konrad.jaeger@hotmail.com>
2025-12-28 14:21:07 +0100

    ff

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/vietnam-trip/",
+  base: "/vietnamTripWebsite/",
   server: {
     host: "::",
     port: 8080,
