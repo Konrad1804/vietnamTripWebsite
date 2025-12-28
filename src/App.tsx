8e7c01c3b5ac210eb27b9b926fb9f67e6c1fe931
Konrad Jäger <konrad.jaeger@hotmail.com>
2025-12-28 14:31:49 +0100

    Fix routes: use RoutePage as start page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
+import RoutePage from "./pages/RoutePage";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<div>APP LÄUFT</div>} />
+      <Route path="/" element={<Navigate to="/route" replace />} />
+      <Route path="/route" element={<RoutePage />} />
+      <Route path="*" element={<Navigate to="/route" replace />} />
     </Routes>
   );
 }
+
